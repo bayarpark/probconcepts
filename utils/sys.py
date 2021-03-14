9b110058a7e6d354b17f83a1a4a4c4cb92d65cac
Bayar Park <bayar342@gmail.com>
2021-03-15 02:18:14 +0700

    check fisher for subrules is added

--- utils/sys.py
+++ utils/sys.py
@@ -2,14 +2,16 @@ from os import mkdir
 from datetime import datetime
 
 
-def makedir(path: str) -> None:
+def makedir(path: str) -> str:
     if not path.endswith('/') or not path.endswith('\\'):
         path = path + '/'
 
     if path.endswith('pcr/'):
-        mkdir(path=f'{path[:1]} {datetime.now().strftime("%Y-%m-%d T %H h")}/')
+        path = f'{path[:1]} {datetime.now().strftime("%Y-%m-%d T %H h")}/'
+        mkdir(path=path)
     else:
         try:
             mkdir(path)
         except FileExistsError:
             pass
+    return path
